705e11374623ddba0415295033a6aae581a3c116
George Scholefield <gscholef@usc.edu>
2025-11-10 11:53:33 -0800

    should be fixed now

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,6 @@ function App() {
       `${statsUrl}?gameName=${name}&tagLine=${tag}`
     );
     const data = await res.json();
-    const text = await res.text();
-    console.log("RAW:", text);
 
     setChampionPool(data.championPoolStats);
     setWeaknesses(data.enemyLanerWins);
